276b8d9adfc1db1b7596d0e5be710df261785f01
santi0ne <johsrodr@espol.edu.ec>
2024-10-04 13:26:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pokemania
+
+A mobile application made in flutter to save your favorite pokemons and upgrade them
---
+++ android/app/src/main/kotlin/com/example/pokemania/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pokemania
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
